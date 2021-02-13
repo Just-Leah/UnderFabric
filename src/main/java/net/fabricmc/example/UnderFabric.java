6834228556fbdb2b9a8c4a0d548eeb70906baaf6
Stonewall0210 <76092465+Stonewall0210@users.noreply.github.com>
2021-02-13 20:02:45 +1000

    Start of toy knife

---
+++ src/main/java/net/fabricmc/example/UnderFabric.java
@@ -0,0 +1,25 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class UnderFabric implements ModInitializer {
+	// Create Items
+	//  Weapons
+	//   Toy Knife
+	public static final Item TOY_KNIFE = new Item(new FabricItemSettings().group(ItemGroup.COMBAT));
+	
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello UnderFabric world!");
+		Registry.register(Registry.ITEM, new Identifier("underfabric", "fabric_item"), TOY_KNIFE);
+	}
+}
